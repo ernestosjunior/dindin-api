36f33d90fb6ca0cc782fbbdc79d0e54daa514154
Ernesto Junior <ernesto.sjunior@hotmail.com>
2021-11-25 00:55:19 -0300

    feat: create login routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRoutes from "./user.routes";
+import loginRoutes from "./login.routes";
 
 const routes: Router = Router();
 
 routes.use("/user", userRoutes);
+routes.use(loginRoutes);
 
 export default routes;
---
+++ src/app/routes/login.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { loginControllers } from "../../app/controllers";
+
+const loginRoutes: Router = Router();
+
+loginRoutes.post("/signin", loginControllers.login);
+
+export default loginRoutes;
