9a97aa40b9c1a146380acb09974ec96cdb491fb2
Ernesto Junior <ernesto.sjunior@hotmail.com>
2021-11-25 01:10:01 -0300

    feat: create login controller

--- src/app/controllers/login.controller.ts
+++ src/app/controllers/login.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { loginSchemas } from "../validations";
 import knexInstance from "../database";
 import { User } from "../database/interfaces";
@@ -21,6 +23,22 @@ export async function login(req: Request, res: Response) {
     if (!user) {
       return res.status(400).json({ success: false, error: "User not found." });
     }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Invalid credentials." });
+    }
+    const apiKey = process.env.API_KEY!;
+    const token = await jwt.sign({ id: user.id }, apiKey, { expiresIn: "1d" });
+
+    const { password: bdPassword, ...restUser } = user;
+
+    return res
+      .status(200)
+      .json({ success: true, data: { user: restUser, token } });
   } catch (error: any) {
     return res.status(500).json({ success: false, error: error.message });
   }
