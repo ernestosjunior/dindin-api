ccc58fc33a1d6a6829434a6ea31903dafde1a5d6
Ernesto Junior <ernesto.sjunior@hotmail.com>
2021-11-24 23:08:44 -0300

    feat: add initial routes

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import routes from "./routes";
 
 dotenv.config();
 
@@ -8,5 +9,6 @@ const app: Express = express();
 
 app.use(cors());
 app.use(json());
+app.use(routes);
 
 export default app;
