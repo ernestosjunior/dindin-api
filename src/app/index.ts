e9226eb76fe5833bb3df12d9dd8606886e159621
Ernesto Junior <ernesto.sjunior@hotmail.com>
2021-11-24 23:06:11 -0300

    chore: add dotenv and cors

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,12 @@
 import express, { Express, json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Express = express();
 
+app.use(cors());
 app.use(json());
 
 export default app;
