04297fbe057e4809507a397d5f3fa0229324f402
Ernesto Junior <ernesto.sjunior@hotmail.com>
2021-11-25 23:57:37 -0300

    refactor: user validation schemas

--- src/app/validations/user.schemas.ts
+++ src/app/validations/user.schemas.ts
@@ -1,14 +1,24 @@
 import joi from './joi'
 
-export const createUser = joi.object({
-  firstName: joi.string().required(),
+export const createUser = joi
+  .object({
+    firstName: joi.string().required(),
+    lastName: joi.string(),
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+    repeatPassword: joi.ref('password'),
+  })
+  .with('password', 'repeatPassword')
+
+export const updateUser = joi.object({
+  firstName: joi.string(),
   lastName: joi.string(),
-  email: joi.string().email().required(),
-  password: joi.string().required(),
-  repeatPassword: joi.ref('password'),
+  email: joi.string().email(),
 })
 
-export const updatePassword = joi.object({
-  password: joi.string().required(),
-  repeatPassword: joi.ref('password'),
-})
+export const updatePassword = joi
+  .object({
+    password: joi.string().required(),
+    repeatPassword: joi.ref('password'),
+  })
+  .with('password', 'repeatPassword')
