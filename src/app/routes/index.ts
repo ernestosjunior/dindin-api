25c56db1e58641bcd5e07a3b3ce73a516bf30723
Ernesto Junior <ernesto.sjunior@hotmail.com>
2021-11-26 21:22:42 -0300

    feat: include default route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,9 +1,21 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
 import userRoutes from './user.routes'
 import loginRoutes from './login.routes'
 import registerRoutes from './register.routes'
 
 const routes: Router = Router()
+
+routes.get('/', async (req: Request, res: Response): Promise<Response> => {
+  return res.status(200).json({
+    success: true,
+    message: {
+      name: 'dindin-api',
+      version: '1.0.0',
+
+      author: 'Ernesto Junior <ernesto.sjunior@hotmail.com>',
+    },
+  })
+})
 
 routes.use(loginRoutes)
 routes.use('/user', userRoutes)
