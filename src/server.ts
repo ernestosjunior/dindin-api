c5a75ad135cdce9499188078e19bdfb437cef802
Ernesto Junior <ernesto.sjunior@hotmail.com>
2023-01-03 01:37:51 -0300

    chore: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import Fastify from 'fastify'
 import * as dotenv from 'dotenv'
+import cors from '@fastify/cors'
 
 dotenv.config()
 
 async function start() {
   const fastify = Fastify({ logger: true })
   const port: number = (process.env.PORT && Number(process.env.PORT)) || 3333
+
+  await fastify.register(cors, { origin: true })
 
   try {
     await fastify.listen({ port })
